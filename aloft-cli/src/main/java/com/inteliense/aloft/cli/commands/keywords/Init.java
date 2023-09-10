fb05ca6f29ae08bd6e16259f733208eb354056db
Ryan Fitzgerald <ryan@inteliense.com>
2023-09-10 17:42:45 -0400

    Various small code changes

---
+++ aloft-cli/src/main/java/com/inteliense/aloft/cli/commands/keywords/Init.java
@@ -0,0 +1,24 @@
+package com.inteliense.aloft.cli.commands.keywords;
+
+import com.inteliense.aloft.cli.Help;
+import com.inteliense.aloft.cli.commands.base.Command;
+import com.inteliense.aloft.cli.commands.base.HandlesCommands;
+import com.inteliense.aloft.cli.config.AppConfig;
+
+public class Init extends HandlesCommands {
+
+    public Init(Command command, AppConfig config) {
+        super(command, config);
+    }
+
+    @Override
+    public void run(AppConfig config) {
+
+    }
+
+    @Override
+    public Help help() {
+        return null;
+    }
+
+}
