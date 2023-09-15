cf38f1d9cff451bf64ec9a8d0d2b0a5c2e35a0d4
Ryan Fitzgerald <ryan@inteliense.com>
2023-09-14 20:39:49 -0400

    Server CLI

--- aloft-server/src/main/java/com/inteliense/aloft/run/cli/config/AppConfig.java
+++ aloft-server/src/main/java/com/inteliense/aloft/run/cli/config/AppConfig.java
@@ -17,37 +17,4 @@ public class AppConfig {
         this.fs = fs;
     }
 
-    public void createTemplate(String projectName) {
-        JSONObject json = new JSONObject();
-
-        JSONObject project = new JSONObject();
-        project.put("name", projectName);
-        project.put("author", "");
-        project.put("description", "");
-        project.put("key", __.hex(Rand.secure(32)));
-        project.put("version", "1.0.0");
-
-        JSONArray modules = new JSONArray();
-        modules.add(buildModule("aloft/tailwindcss", "git://path/to/repo", "1.0.0"));
-        modules.add(buildModule("aloft/base-components", "git://path/to/repo", "1.0.0"));
-        modules.add(buildModule("aloft/crypto", "git://path/to/repo", "1.0.0"));
-
-        json.put("project", project);
-        json.put("modules", modules);
-        try {
-            fs.printConfig(projectName, JSON.getString(json, true));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private JSONObject buildModule(String name, String repo, String version) {
-        JSONObject obj = new JSONObject();
-        JSONObject mod = new JSONObject();
-        mod.put("git", repo);
-        mod.put("version", version);
-        obj.put(name, mod);
-        return obj;
-    }
-
 }
