6d35188782fa416005ee9f0eb551b00e07b54451
Ryan Fitzgerald <ryan@inteliense.com>
2023-09-17 23:26:20 -0400

    Http header processing

--- aloft-server/src/main/java/com/inteliense/aloft/compiler/application/config/AppConfig.java
+++ aloft-server/src/main/java/com/inteliense/aloft/compiler/application/config/AppConfig.java
@@ -1,9 +1,5 @@
 package com.inteliense.aloft.compiler.application.config;
 
-import com.inteliense.aloft.server.http.middleware.base.Middleware;
-
-import java.util.ArrayList;
-
 public class AppConfig {
 
     //SERVER CONFIGURATION
--- aloft-server/src/main/java/com/inteliense/aloft/server/http/supporting/HeaderList.java
+++ aloft-server/src/main/java/com/inteliense/aloft/server/http/supporting/HeaderList.java
@@ -1,4 +1,13 @@
 package com.inteliense.aloft.server.http.supporting;
 
+import java.util.ArrayList;
+
 public class HeaderList {
+
+    private ArrayList<Header> headers = new ArrayList<>();
+
+    public void addHeader(Header header) {
+        headers.add(header);
+    }
+
 }
